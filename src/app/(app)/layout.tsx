21023ccd29664a301421d035747e004b4a399173
doridori-samsam <wtawonlife@gmail.com>
2026-02-04 11:27:58 +0900

    💄전역 theme 및 로고 파일 추가

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,3 @@
+function AppLayout({ children }: { children: React.ReactNode }) {
+  return <main>app 레이아웃</main>;
+}
---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,10 @@
+function AuthLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <main>
+      <h1>Auth Layout</h1>
+      {children}
+    </main>
+  );
+}
+
+export default AuthLayout;
