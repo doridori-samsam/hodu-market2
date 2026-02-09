3b47321a2844eaae8ebda6f864cf1e1b74d3aff1
doridori-samsam <wtawonlife@gmail.com>
2026-02-09 10:14:36 +0900

    🔧 eslint import 순서 관련 라이브러리 추가 및 설정 세팅

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import eslintPluginImport from "eslint-plugin-import";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +14,85 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    plugins: {
+      import: eslintPluginImport,
+    },
+    settings: {
+      "import/resolver": {
+        typescript: true,
+      },
+    },
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling", "index"],
+            "object",
+            "type",
+          ],
+          pathGroups: [
+            {
+              pattern: "react",
+              group: "external",
+              position: "before",
+            },
+            {
+              pattern: "react*",
+              group: "external",
+              position: "before",
+            },
+            {
+              pattern: "next/**",
+              group: "external",
+              position: "before",
+            },
+            {
+              pattern: "@/app/page*",
+              group: "internal",
+              position: "after",
+            },
+            {
+              pattern: "@/components/**",
+              group: "internal",
+              position: "after",
+            },
+            {
+              pattern: "@/constants/**",
+              group: "internal",
+              position: "after",
+            },
+            {
+              pattern: "@/types/**",
+              group: "internal",
+              position: "after",
+            },
+            {
+              pattern: "*.css",
+              group: "type",
+              position: "after",
+            },
+            {
+              pattern: "**/*.css",
+              group: "type",
+              position: "after",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["react"],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+          warnOnUnassignedImports: true,
+        },
+      ],
+    },
+  },
 ]);
 
 export default eslintConfig;
